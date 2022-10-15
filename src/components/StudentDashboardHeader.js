374731dc7537105cbdf3f2d85e533c7c6504735b
MJ Kim <mjk@missionreadyhq.com>
2022-10-15 14:55:29 +1300

    added studentdashboard components

---
+++ src/components/StudentDashboardHeader.js
@@ -0,0 +1,58 @@
+import "./StudentDashboardHeader.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="left">
+        <img
+          src="NavBar/LevelUpWorks-blue.png"
+          alt="Level Up Logo"
+          className="logo"
+        />
+
+        <div className="projectName">
+          <h3 className="h3">PROJECT</h3>
+          <p className="p">Introduction</p>
+        </div>
+      </div>
+
+      <div className="center">
+
+      <div className="circle">1</div>
+
+      <div className="mini-circle"></div>
+      <div className="mini-circle"></div>
+      <div className="mini-circle"></div>
+      <div className="mini-circle"></div>
+      <div className="mini-circle"></div>
+      <div className="mini-circle"></div>
+      <div className="mini-circle"></div>
+      <div className="mini-circle"></div>
+      <div className="mini-circle"></div>
+      <div className="mini-circle"></div>
+      <div className="mini-circle"></div>
+      <div className="mini-circle"></div>
+      <div className="mini-circle"></div>
+      <div className="mini-circle"></div>
+      </div>
+
+      <div className="right">
+        <button className="screen-btn"> Take Screenshot </button>
+        <button className="help-btn"> Ask Teacher for help </button>
+        <button className="projects-btn"> More Projects </button>
+
+        <div className="flagimages">
+          <img src="NavBar/NZflag.png" alt="nzFlag" className="nzflag" />
+
+          <img
+            src="NavBar/Maoriflag.png"
+            alt="Maoriflag"
+            className="maoriflag"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
