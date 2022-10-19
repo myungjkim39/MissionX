8fbc7f0fff8890d4bb93e0157631e08c79aa8d28
MJ Kim <mjk@missionreadyhq.com>
2022-10-19 13:35:43 +1300

    changed studentdashvideotutorial

--- src/pages/studentdashboard/StudentDashBoardMakeProject.js
+++ src/pages/studentdashboard/StudentDashBoardMakeProject.js
@@ -1,5 +1,4 @@
 import React from 'react'
-
 import Header from "../../components/StudentDashboardHeader.js";
 import Project from "../../components/StudentDashProjectContent.js";
 import Footer from "../../Shared components/Footer.js";
