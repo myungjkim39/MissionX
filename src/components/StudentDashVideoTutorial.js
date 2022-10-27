69a8661e5feaf394e6ec084cebad4a1230a3a4de
MJ Kim <mjk@missionreadyhq.com>
2022-10-27 13:11:34 +1300

    updating

--- src/components/StudentDashVideoTutorial.js
+++ src/components/StudentDashVideoTutorial.js
@@ -9,11 +9,21 @@ export default function VideoTutorial() {
       <div id="contentbox">
         <div id="innerbox">
           <div className="content">
+            <div className="blocks">
+              <div className="left-box">
+              <div className="arrow-circle" />
+              <p4><i class="arrow-left"></i></p4>
+              </div>
             <img
               src="/src-assets/StudentDashboard/videotutorial-screenshot.png"
               alt="project"
               className="video-screenshot"
-            />
+              />
+              <div className="right-box">
+                <div className="arrow-circle" />
+                <p4><i class="arrow-right"></i></p4>
+              </div>
+              </div>
           </div>
         </div>
       </div>
--- src/pages/studentdashboard/StudentDashboardInstructions.js
+++ src/pages/studentdashboard/StudentDashboardInstructions.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import Header from "../../components/StudentDashboardHeader.js";
+
+import Footer from "../../Shared components/Footer.js";
 
 export default function StudentDashboardInstructions() {
   return (
-    <div>StudentDashboardInstructions</div>
+    <div className= "student-dashboard-instructions">
+      < Header />
+      < Footer />
+    </div>
   )
 }
