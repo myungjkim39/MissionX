610aaadc1a540a0c590fc04aa50af6f84ba998ef
Sneha Choudhary <snehac@missionreadyhq.com>
2022-10-28 17:29:16 +0530

    28th oct final

--- frontend/src/pages/TeacherProfile.js
+++ frontend/src/pages/TeacherProfile.js
@@ -1,8 +1,9 @@
 import React, {useEffect, useState} from 'react'
 import '../components/TeacherProfileViewer.css'
 import { Link } from 'react-router-dom';
-import Footer from '../Shared components/Footer.js';
+import Footer from '../components/homecomponents/Footer.js';
 import Header from '../components/TeacherProfileHeader';
+import And from '../images/AidenAndrews.png';
 
 function TeacherProfileViewer() {
   const [teacher, setTeacher] = useState()
@@ -14,20 +15,35 @@ function TeacherProfileViewer() {
       <Header />
         <div className="teacher-profile-side">
         <div className="teacherprofile-options">
-          <img src={teacher.profilePic} width="width: 59px" alt="T Image" /> <br />
+          <img src= {teacher.profilePic} width="width: 59px" alt="T Image" /> <br />
             <button type="button" className="teacher-profile-btn1">EDIT PROFILE</button>
             <button type="button" className="teacher-profile-btn1">CHANGE PHOTO</button>
             <button type="button" className="teacher-profile-btn1">SETTINGS</button>
             </div>
 
-            <div className="teacher-info">
+        <div className="teacher-info">
+          
           {teacher && (<>
-          <h3>{teacher.name}</h3>
-          <p>School {teacher.school}</p>
-          <p>Date of Birth {teacher.dateOfBirth}</p>
-          <p>Contact No {teacher.contactNumber}</p>
-          <p>Email Address {teacher.email}</p>
+            
+            <h3>{teacher.name}</h3>
+            
+            <div className="teacherprofileinfo-mainschool">
+            <div className="teacherprofileinfo-school">
+              <h4>School</h4>
+              <h4>Date of Birth</h4> 
+                <h4>Contact No</h4> 
+                 <h4>Email Address</h4> 
+              </div>
+
+            <div className="teacherprofileinfo-school2">
+              <p>{teacher.school}</p>
+                <p>{teacher.dateOfBirth}</p>
+                <p>{teacher.contactNumber}</p>
+                 <p>{teacher.email}</p>
+              </div>
+              </div>
           </>)}
+
           </div>
         </div>
         <div className="dashboard-projects">
