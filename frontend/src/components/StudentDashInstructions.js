4d459ef1f8348a9e856f6e4840b1ee6a6afa91e6
MJ Kim <mjk@missionreadyhq.com>
2022-11-03 09:04:13 +1300

    added links to studentdashboard

--- frontend/src/components/StudentDashInstructions.js
+++ frontend/src/components/StudentDashInstructions.js
@@ -9,6 +9,14 @@ export default function StudentProject() {
       <div className="content-box">
         <div className="inner-box">
           <div className="student-content">
+            
+            <b>1. Join Scratch </b>
+            <p>If you haven't used Scratch before, you will need to join Scratch first.</p>
+            <p>Go to https://scratch.mit.edu. Click on Join Scratch.</p>
+            <img src= "/public-images/projects/Project01-instructions.png" alt= "instructions"/>
+            <p>Follow the instructions to join. You will need a username and a password that you will remember. If possible,</p>
+            <p>you should also verify your email address so that you can Share projects later. Ask your teacher to help with</p>
+            <p>this step if you don't have an email address, or if you are not sure what to do.</p>
 
           </div>
         </div>
